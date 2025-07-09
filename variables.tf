bff11e8e88a4d5b1149a87e64cd3ca40ca3ddd53
shanthi <shanbojja@gmail.com>
2025-07-10 04:58:18 +0530

    ansible-terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z03468932B7WI1KW96CYF"
+}
+
+variable "zone_name" {
+    default = "daws-shankran.site"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
